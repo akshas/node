f3c386c69b9b06fcb8a1859745ebe3aae83ca5f7
Alex Kochetov <allexxx81@gmail.com>
2018-05-20 23:50:17 +0200

    inheritance of errors

--- error.js
+++ error.js
@@ -1,13 +1,35 @@
 var util = require('util');
 var db = require('db');
-// db.connect();
-var serv = require('server');
+db.connect();
+// var serv = require('server');
+
+class HttpError extends Error{
+  constructor(status, message) {
+    super(message);
+    this.status = status;
+    Error.captureStackTrace(this, HttpError);
+  }
+}
+
 function makePage(url){
   if(url != 'index.html') {
-    throw new Error('Нет такой страницы');
+    throw new HttpError(404,'Нет такой страницы');
   }
-  return util.format('%s, %s!', db.getPhrases('Hallo'), db.getPhrases('world'));
-
+    return util.format('%s, %s!', db.getPhrases('Hallo'), db.getPhrases('world'));
 }
-var page = makePage('index.html');
-console.log(page);
+
+try {
+  var page = makePage('index.html');
+  console.log(page);
+} catch (error) {
+  if (error instanceof HttpError) {
+    console.log(`${error.status} ${error.message}`);
+  }
+  // else if (error instanceof ParseError) {
+  //   console.log(`${error.status} ${error.message} ${error.name} ${error.stack}`);
+
+  // }
+  else{
+    console.error(`Ошибка ${error.message} - "${error.name}" ${error.stack}`);
+  }
+}
